04ce021e5a54c9359992a284554b8e04136b9ee2
r0cd7b <tkdrbs1928@naver.com>
2020-11-29 13:47:07 +0900

    Algorithm was modified.

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -3,10 +3,17 @@ import random
 
 def generate_password():
     try:
+        special = "!$%*?"
+        decimal = "0123456789"
+        upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        lower = "abcdefghijklmnopqrstuvwxyz"
+        characters = special + decimal + upper + lower
         while input() == "":
-            password = ""
-            for i in range(12):
-                password += random.choice("!$%*0123456789?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+            password = random.choice(special) + random.choice(decimal) + random.choice(upper) + random.choice(lower)
+            for i in range(8):
+                password += random.choice(characters)
+            password = list(password)
+            random.shuffle(password)
             print(password)
     except Exception as e:
         print(e)
